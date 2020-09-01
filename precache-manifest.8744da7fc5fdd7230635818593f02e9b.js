b501817bcc635c30f8cdfacced08b3c132aa29bb
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-09-01 17:07:34 -0400

    Updates

---
+++ precache-manifest.8744da7fc5fdd7230635818593f02e9b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabae640bd3df57ba9150e5ff11dae98",
+    "url": "/yasamanloghmani/tailwood.git/index.html"
+  },
+  {
+    "revision": "2ca505d2da51ef823360",
+    "url": "/yasamanloghmani/tailwood.git/static/css/main.9e2f3d3d.chunk.css"
+  },
+  {
+    "revision": "fef063f8cc6211102831",
+    "url": "/yasamanloghmani/tailwood.git/static/js/2.76d452aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yasamanloghmani/tailwood.git/static/js/2.76d452aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ca505d2da51ef823360",
+    "url": "/yasamanloghmani/tailwood.git/static/js/main.bbfac0e4.chunk.js"
+  },
+  {
+    "revision": "0622aaaaaf60c9386e14",
+    "url": "/yasamanloghmani/tailwood.git/static/js/runtime-main.e77d1201.js"
+  },
+  {
+    "revision": "195645b5bd6f021915e82efee4607343",
+    "url": "/yasamanloghmani/tailwood.git/static/media/maindashboard.195645b5.png"
+  },
+  {
+    "revision": "5fdf3c11e4eeb3cc6e602e78f1a0c91b",
+    "url": "/yasamanloghmani/tailwood.git/static/media/marketing.5fdf3c11.png"
+  },
+  {
+    "revision": "33cbd5fcaaf5936a9b2bbdff27d69298",
+    "url": "/yasamanloghmani/tailwood.git/static/media/marketingimg.33cbd5fc.png"
+  },
+  {
+    "revision": "1474c6b6742057f4c52d19211dca4454",
+    "url": "/yasamanloghmani/tailwood.git/static/media/mintbean.1474c6b6.png"
+  },
+  {
+    "revision": "c7c37cd2bac002518cb64d7dd501fc69",
+    "url": "/yasamanloghmani/tailwood.git/static/media/tailwind.c7c37cd2.png"
+  }
+]);
