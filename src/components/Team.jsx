4ac46cba2d1ab336c01ad59d7fcf66434f48d158
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-09-02 10:44:39 -0400

    changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,7 +5,7 @@ class Team extends Component{
         return(
             <div class="grid grid-cols-2  border-4 rounded-full border-falling-gray p-8 m-32">
                 <div class="md:flex-shrink-0 px-6 ">
-                    <img class="rounded-lg w-400 p-4" src={this.props.teamimg} alt="Woman paying for a purchase" />
+                    <img class="rounded-full w-400 p-4" src={this.props.teamimg} alt="Woman paying for a purchase" />
                 </div>
                 <div>
                 <div class="font-bold text-3xl mb-2 text-center text-blue-light">{this.props.h}</div>    
