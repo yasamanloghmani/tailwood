84d04da52e5601130a4afa66b28bbbd1c400f205
Yasaman Loghmani <yasamanloghmani@Yasamans-MacBook-Pro.local>
2020-09-02 10:55:21 -0400

    Add margin

--- src/components/SectionWImage.jsx
+++ src/components/SectionWImage.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SectionWImage extends Component{
     render(){
         return(
-            <div class="grid grid-cols-2 mx-32 shadow-boxshadow">
+            <div class="grid grid-cols-2 mx-32 shadow-boxshadow p-6">
                 <div class="md:flex-shrink-0 px-6 ">
                     <img class="rounded-lg w-400 shadow-2xl p-4" src={this.props.cardImage} />
                 </div>
